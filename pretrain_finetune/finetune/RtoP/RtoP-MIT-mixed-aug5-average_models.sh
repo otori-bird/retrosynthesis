cd2d7d5f22d2ea598766ab2f1fb9174b78196797
zzp <otori_bird@zju.edu.cn>
2022-06-22 07:19:51 +0000

    update config

--- pretrain_finetune/finetune/RtoP/RtoP-MIT-mixed-aug5-average_models.sh
+++ pretrain_finetune/finetune/RtoP/RtoP-MIT-mixed-aug5-average_models.sh
@@ -1,6 +1,6 @@
-onmt_average_models -output  ./exp/USPTO-MIT_RtoP_aug5_mixed/average_model_96-100.pt   \
-    -m  exp/USPTO-MIT_RtoP_aug5_mixed/model.reactants-products_step_960000.pt \
-        exp/USPTO-MIT_RtoP_aug5_mixed/model.reactants-products_step_970000.pt \
-        exp/USPTO-MIT_RtoP_aug5_mixed/model.reactants-products_step_980000.pt \
-        exp/USPTO-MIT_RtoP_aug5_mixed/model.reactants-products_step_990000.pt \
-        exp/USPTO-MIT_RtoP_aug5_mixed/model.reactants-products_step_1000000.pt
+onmt_average_models -output  ./exp/USPTO-MIT_RtoP_aug5_mixed/finetuned_average_model_96-100.pt   \
+    -m  exp/USPTO-MIT_RtoP_aug5_mixed/finetuned_model.reactants-products_step_960000.pt \
+        exp/USPTO-MIT_RtoP_aug5_mixed/finetuned_model.reactants-products_step_970000.pt \
+        exp/USPTO-MIT_RtoP_aug5_mixed/finetuned_model.reactants-products_step_980000.pt \
+        exp/USPTO-MIT_RtoP_aug5_mixed/finetuned_model.reactants-products_step_990000.pt \
+        exp/USPTO-MIT_RtoP_aug5_mixed/finetuned_model.reactants-products_step_1000000.pt
